28c6bf744d3ddf1a4bb7b6bd3b6daf5a6d4d2059
N. G. Scheurich <ngscheurich@gmail.com>
2018-02-06 17:37:19 -0600

    Formatting

--- 2_a-whirlwind-tour/lib/a_whirlwind_tour.ex
+++ 2_a-whirlwind-tour/lib/a_whirlwind_tour.ex
@@ -1,14 +1,14 @@
 defmodule AWhirlwindTour do
-	# Exercise 2.9.1:
-	#
+  # Exercise 2.9.1:
+  #
   # 	Implement `sum/1`. This function should take in a list
   # 	of numbers and return the sum of the list.
   def sum(list) do
     Enum.reduce(list, fn(x, acc) -> x + acc end)
   end
 
-	# Exercise 2.9.3a
-	#
+  # Exercise 2.9.3a
+  #
   # 	Transform `[1,[[2],3]]` to `[9, 4, 1]` with the
   # 	pipe operator. 
   def pipe_transform(list) do
@@ -18,9 +18,9 @@ defmodule AWhirlwindTour do
     |> Enum.map(fn x -> x * x end)
   end
 
-	# Exercise 2.9.3b
-	#  
-	#		Transform `[1,[[2],3]]` to `[9, 4, 1]` without the
+  # Exercise 2.9.3b
+  #  
+  #	Transform `[1,[[2],3]]` to `[9, 4, 1]` without the
   # 	pipe operator. 
   def transform(list) do
     list = List.flatten(list)
@@ -29,17 +29,17 @@ defmodule AWhirlwindTour do
   end
 
 
-	# Exercise 2.9.4:
-	#
+  # Exercise 2.9.4:
+  #
   # 	Translate `crypto:hash(md5, "Tales from the Crypt").`
   # 	from Erlang to Elixir.
   def erlang_to_elixir do
     :crypto.hash(:md5, "Tales from the Crypt")
   end
 
-	# Exercise 2.9.4:
-	#
+  # Exercise 2.9.4:
+  #
   # 	Take a look at an IPV4 packet. Try writing a parser for it.
-	#
-	# 	STUDY https://forums.manning.com/posts/list/35976.page	
+  #
+  # 	STUDY https://forums.manning.com/posts/list/35976.page	
 end
